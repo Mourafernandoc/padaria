94f56de7d5619e1bea9d52f7164e8fd53bbe8cc2
skyennonet <skyennonet@gmail.com>
2024-10-25 17:04:52 -0300

    secound commit

--- padaria/core/urls.py
+++ padaria/core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('produtos/', views.listar_produtos, name='listar_produtos'),
     path('produtos/novo/', views.cadastrar_produto, name='cadastrar_produto'),
+    path('produtos/editar/<int:pk>/', views.editar_produto, name='editar_produto'),
 ]
--- padaria/core/views.py
+++ padaria/core/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_list_or_404
 from .models import Produto
 from .forms import ProdutoForm
 
 # Create your views here.
+
+def editar_produto(request, pk):
+    produto = get_list_or_404(Produto, pk=pk)
+
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST, instance=produto)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_produtos')
+    else:
+        form = ProdutoForm(instance=produto)
+
+    return render(request, 'core/editar_produto.html', {'form': form, 'produto': produto})
+
+
 def listar_produtos(request):
     produtos = Produto.objects.all()
     return render(request, 'core/listar_produtos.html', {'produtos': produtos})
